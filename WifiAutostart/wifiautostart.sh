03353f298eaa0214b71e62ec2a6545000cba90c1
LeSnake04 <47243761+LeSnake04@users.noreply.github.com>
2021-02-10 15:53:50 +0100

    update

---
+++ WifiAutostart/wifiautostart.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -v
+#
+# WifiAutostart by LeSnake04
+# Version 1.0 
+#
+# Code under GPL 3 License: https://www.gnu.org/licenses/gpl-3.0.html
+
+
+# General Wifi 
+PROGNAME="WifiAutostart"
+
+#Enable Networking
+nmcli n on
+
+#Get SSID of Wifi
+getwifiname (){
+	WIFINAME=$(iw dev | grep ssid | sed -e 's/		ssid //g')
+}
+
+getwifiname
+# Repeat Checking SSID until connected to wifi
+while [ "${WIFINAME}" = "" ]; do
+	echo "No active wifi conection"
+	sleep 2
+	getwifiname
+done
+
+echo "Found Wifi Connection:${WIFINAME}"
+notify-send $PROGNAME "Starting Actions for Wifi $WIFINAME ..."
+
+# Actions to perform
+case $WIFINAME in 
+#	Example:
+#	"HomeWifiSSID" | "FriendsWifiSSID" ) # Wifi SSid to trigger actions (case sensitive)
+# 		# Programs to Start (Add '&'' at the end of each command)
+#		telegram-desktop&
+#		nextcloud&	
+#
+#
+	"") 
+		
+	;;
+	*)
+		notify-send $PROGNAME "No programms to Start for this wifi"
+esac
